8e3f9f59b8c961904262f07cbaa71bd8cc77ee2b
OnUr-KB <kkanya0947@gmail.com>
2024-02-23 15:55:53 +0700

    update

--- Equation.h
+++ Equation.h
@@ -2,28 +2,46 @@
 #include<string>
 using namespace std;
 
-class RectilinearMotion {
+class equation {
     private:
-        string Displacement = "s"; //การกระจัด
-        string Velocity = "v"; //ความเร็ว
-        string Time = "t"; //เวลา
+        string fomula  ;
+        string description ;
+        string tag ;
+        double Displacement;
+        double Velocity;
+        double Time;
 
     public:
-        string fomula = "v = s / t";
-        string description = "Use to find velocity";
-        string tag = "Motion";
+        void Motion(double,double,double);
+        void Area(double,double,double);
+
+
 };
 
-class Area {
-    private:
-       string AreaOfCricle = "A"; //พื้นที่วงกลม
-       string Radius = "r"; //รัศมี
-       string Pi = "3.14"; //ค่าPi
+void equation::Motion(double s,double v,double t){
+    Displacement = s;
+    Velocity = v;
+    Time = t ;
+    string fomula = "v = s / t" ;
+    string description = "Use to find velocity";
+    string tag = "Motion" ;
     
-    public:
-       string fomula = "Pi * r * r";
-       string description = "Use to find AreaOfCricle";
-       string tag = "Area";
-};
+}
+
+void equation::Area(double A,double r,double &Pi){
+}
+
+// class Area {
+//     // private:
+//     //    string AreaOfCricle = "A"; //พื้นที่วงกลม
+//     //    string Radius = "r"; //รัศมี
+//     //    string Pi = "3.14"; //ค่าPi
+    
+//     public:
+//        string fomula = "Pi * r * r";
+//        string description = "Use to find AreaOfCricle";
+//        string tag = "Area";
+// };
+
 
 
