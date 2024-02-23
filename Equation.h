b8289a17637e093aa36a3f701577e95b0538ca50
OnUr-KB <kkanya0947@gmail.com>
2024-02-23 14:10:10 +0700

    update

--- Equation.h
+++ Equation.h
@@ -1,9 +1,18 @@
 #include<iostream>
-#include<cmath>
 #include<string>
 using namespace std;
 
-class variable {
+class RectilinearMotion {
+    private:
+        string Displacement = "s"; //การกระจัด
+        string Velocity = "v"; //ความเร็ว
+        string Time = "t"; //เวลา
+
     public:
-          
-}
+        string fomula = "v = s / t";
+        string description = "use to find velocity";
+        string tag = "Motion";
+        // string eq2 = "s = v * t";
+        // string description = "use to find displacement";
+        // string tag = "Motion";
+};
