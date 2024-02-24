40bfb477ff905c111ede18ecd1f3beb0bc2aaf7d
archiash <46347714+archiash@users.noreply.github.com>
2024-02-24 18:51:34 +0700

    Add Function Support and Negative Sign - Trigonometry (sin, cos, tan, csc, sec, cot) - Logarithm (base e, base 10) - Negtive Sign ("A = -B", "A = -(B)", "A = -ln(B)", ...)

--- Calcula/Pec/Calculate.cpp
+++ Calcula/Pec/Calculate.cpp
@@ -1,12 +1,41 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+//#include <cmath>
 #include <iostream>
-#include <cmath>
 #include <iomanip>
 #include <vector>
 #include <string>
 #include <unordered_map>
 using namespace std;
-
-double Calculate(string problem)
+void ToLowerCase(string &s)
+{
+    string r = "";
+    for (auto i : s)
+    {
+        r += tolower(i);
+    }
+    s = r;
+}
+double Function(string func, double value)
+{
+    ToLowerCase(func);
+    int sign = 1;
+    double result = value;
+    if (func.size() > 0) if (func[0] == '-') { 
+        sign = -1; 
+        func.replace(0, 1, "");
+    }
+    if (func == "sin" || func == "sine") result = sin(value * M_PI / 180.0L);
+    else if (func == "cos" || func == "cosine") result = cos(value * M_PI / 180.0L);
+    else if (func == "tan" || func == "tangent") result = tan(value * M_PI / 180.0L);
+    else if (func == "cosec" || func == "cosecant" || func == "csc") result = 1.0L / sin(value * M_PI / 180.0L);
+    else if (func == "sec" || func == "secant") result = 1.0L / cos(value * M_PI / 180.0L);
+    else if (func == "cot" || func == "cotan" || func == "cotangent") result = 1.0L / tan(value * M_PI / 180.0L);
+    else if (func == "ln") result = log(value);
+    else if (func == "log") result = log10(value);
+    return sign * result;
+}
+double Calculate(string problem, string function = "")
 {
     vector<string> oparationList;
     vector<double> numList;
@@ -19,16 +48,25 @@ double Calculate(string problem)
         {
             if (oparation[i] == '(')
             {
+                string func = "";
+                if (alphabet.size() > 0)
+                {
+                    func = alphabet;
+                    alphabet = "";
+                }
                 i++;
                 string subProblem = "";
                 int subParenthesis = 0;
-                while (oparation[i] != ')' || subParenthesis > 0) {
-                    if (oparation[i] == '(') subParenthesis++;
-                    if (oparation[i] == ')') subParenthesis--;
+                while (oparation[i] != ')' || subParenthesis > 0)
+                {
+                    if (oparation[i] == '(')
+                        subParenthesis++;
+                    if (oparation[i] == ')')
+                        subParenthesis--;
                     subProblem += oparation[i];
                     i++;
                 }
-                numList.push_back(Calculate(subProblem));
+                numList.push_back(Calculate(subProblem, func));
                 continue;
             }
             alphabet += oparation[i];
@@ -38,7 +76,10 @@ double Calculate(string problem)
         {
             if (alphabet.size() > 0)
             {
-                if (alphabet[0] >= '0' && alphabet[0] <= '9')
+                if (alphabet[0] == '-' && (alphabet[1] >= '0' && alphabet[1] <= '9')) {
+                    numList.push_back(stod(alphabet));
+                }
+                else if (alphabet[0] >= '0' && alphabet[0] <= '9')
                     numList.push_back(stod(alphabet));
                 else
                     oparationList.push_back(alphabet);
@@ -94,38 +135,52 @@ double Calculate(string problem)
         }
     }
 
-    return numList[0];
+    return Function(function, numList[0]);
 }
 
-static vector<string> GetInputVariablesList(string formula) {
+static vector<string> GetInputVariablesList(string formula)
+{
     vector<string> variables;
     string var = "";
-    for (auto i : formula) {
-        if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z')) {
+    for (auto i : formula)
+    {
+        if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+        {
             var += i;
         }
-
-        if (i == ' ' || i == '+' || i == '-' || i == '*' || i == '/' || i == '^' || i == ')' || i == '(') {
+        if (i == '(') {
+            var = "";
+        }
+        if (i == ' ' || i == '+' || i == '-' || i == '*' || i == '/' || i == '^' || i == ')')
+        {
             if (var != "")
                 variables.push_back(var);
             var = "";
         }
     }
-    if (var != "") variables.push_back(var);
+    if (var != "")
+        variables.push_back(var);
     return variables;
 }
- 
-static double CalcualteEquation(string formula, unordered_map<string, float> variables) {
+
+static double CalcualteEquation(string formula, unordered_map<string, double> variables)
+{
     formula = formula.substr(formula.find("=") + 1);
     string var = "";
     cout << formula << endl;
-    for (int i = 0; i < formula.size(); i++) {
-        if ((formula[i] >= 'a' && formula[i] <= 'z') || (formula[i] >= 'A' && formula[i] <= 'Z')) {
+    for (int i = 0; i < formula.size(); i++)
+    {
+        if ((formula[i] >= 'a' && formula[i] <= 'z') || (formula[i] >= 'A' && formula[i] <= 'Z'))
+        {
             var += formula[i];
         }
-
-        if (formula[i] == ' ' || formula[i] == '+' || formula[i] == '-' || formula[i] == '*' || formula[i] == '/' || formula[i] == '^' || formula[i] == ')' || formula[i] == '(') {
-            if (var != "") {
+        if (formula[i] == '(') {
+            var = "";
+        }
+        if (formula[i] == ' ' || formula[i] == '+' || formula[i] == '-' || formula[i] == '*' || formula[i] == '/' || formula[i] == '^' || formula[i] == ')')
+        {
+            if (var != "")
+            {
                 cout << var << endl;
                 formula.replace(i - var.size(), var.size(), to_string(variables[var]));
                 cout << formula << endl;
@@ -133,7 +188,8 @@ static double CalcualteEquation(string formula, unordered_map<string, float> var
             var = "";
         }
     }
-    if (var != "") {
+    if (var != "")
+    {
         cout << var << endl;
         formula.replace(formula.size() - var.size(), var.size(), to_string(variables[var]));
         cout << formula << endl;
