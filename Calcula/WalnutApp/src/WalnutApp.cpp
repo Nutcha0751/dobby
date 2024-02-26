6091a39879ba5a25ae078a01e6cc2f24ed2cc706
Mosjitos <kamolrat_saeboon@cmu.ac.th>
2024-02-26 11:38:49 +0700

    hide circle

--- Calcula/WalnutApp/src/WalnutApp.cpp
+++ Calcula/WalnutApp/src/WalnutApp.cpp
@@ -22,9 +22,9 @@ public:
 	string resultValue;
 	string resultVariable;
 	int menu = 0;
-	float PX;
-	float PY;
-	float R;
+	//float PX;
+	//float PY;
+	//float R;
 	virtual void OnAttach() {
 		image = std::make_shared<Walnut::Image>("GR.jpg");
 	}
@@ -37,19 +37,19 @@ public:
 		ImVec2 screenSize = ImGui::GetIO().DisplaySize;
 		background->AddRectFilledMultiColor(screen, ImVec2(screen.x + screenSize.x, screenSize.y / 2 + screen.y), ImColor(255, 255, 204, 255), ImColor(255, 255, 204, 255), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 255));
 		background->AddRectFilledMultiColor(ImVec2(screen.x, screenSize.y / 2 + screen.y), ImVec2(screen.x + screenSize.x, screenSize.y + screen.y), ImColor(255, 255, 255, 255), ImColor(255, 255, 255, 255), ImColor(255, 229, 204, 255), ImColor(255, 229, 204, 255));
-		background->AddCircleFilled(ImVec2(screen.x + screenSize.x / 2 + PX, screenSize.y / 2 + screen.y + PY), R, ImColor(255, 0, 0, 255));
+		//background->AddCircleFilled(ImVec2(screen.x + screenSize.x / 2 + PX, screenSize.y / 2 + screen.y + PY), R, ImColor(255, 0, 0, 255));
 		//background->AddImage(image->GetDescriptorSet(), screen, ImVec2(screen.x + screenSize.x, screenSize.y + screen.y), ImVec2(0.0f, 0.0f), ImVec2(1.0f, 1.0f), ImColor(255,255,255,100));
 		ImGui::Text("");
 		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
-		ImGui::SliderFloat("P.X", &PX,-900,900);
+		//ImGui::SliderFloat("P.X", &PX,-900,900);
 		ImGui::PopStyleColor();
 
 		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
-		ImGui::SliderFloat("P.Y", &PY, -300, 300);
+		//ImGui::SliderFloat("P.Y", &PY, -300, 300);
 		ImGui::PopStyleColor();
 
 		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
-		ImGui::SliderFloat("R", &R,0,1000);
+		//ImGui::SliderFloat("R", &R,0,1000);
 		ImGui::PopStyleColor();
 
 		ImGui::Columns(3, "MyLayout", false);
