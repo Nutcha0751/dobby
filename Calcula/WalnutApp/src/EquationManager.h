ae450c2ebe99356a190843a31e7a341b04c89e30
OnUr-KB <kkanya0947@gmail.com>
2024-02-27 13:03:59 +0700

    update

--- Calcula/WalnutApp/src/EquationManager.h
+++ Calcula/WalnutApp/src/EquationManager.h
@@ -12,16 +12,14 @@ public:
 		vector<string> equations;
 		string formula;
 		string description;
-		string tag;
-		while (getline(file, formula, ',') && getline(file, description, ',') && getline(file, tag)) {
+		while (getline(file, formula, ',') && getline(file, description, ',')) {
 			equations.push_back(formula);
 			equations.push_back(description);
-			equations.push_back(tag);
 		}
 		return equations;
 	}
 
-	static void SaveEquations(vector<string>& formula, vector<string>& description, vector<string>& tag) {
+	static void SaveEquations(vector<string>& formula, vector<string>& description) {
 		ofstream file;
 		file.open("equation.txt");
 		for (int i = 0; i < formula.size(); i++) {
@@ -29,11 +27,7 @@ public:
 			if (i < description.size()) {
 				file << "Description: " << description[i] << endl;
 			}
-			if (i < tag.size()) {
-				file << "Tag: " << tag[i] << endl;
-			}
 			file << endl << endl;
 		}
 	}
 };
-
